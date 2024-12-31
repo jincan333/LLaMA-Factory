bf28cfcc48fc074df5e16da3a000fe96ee12b649
jincan333 <jincan333@gmail.com>
2024-12-31 16:15:53 -0500

    debug

---
+++ run/llama3_gsm8k_sft_dtai.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -N 1             
+#SBATCH -t 3:00:00
+#SBATCH --partition=ghx4
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 64
+#SBATCH --gpus-per-node 4
+#SBATCH -J llama3_gsm8k_sft
+echo "SLURM_JOB_ID: ${SLURM_JOB_ID}"
+echo "Current directory: $(pwd)"
+echo "Job started at $(date)"
+date_str=$(date '+%S_%d_%b_%Y')
+echo "Running job on $(hostname)"
+echo "Job started at $(date)"
+
+source ~/.bashrc
+prefix="llama3_gsm8k_sft"
+time srun --label apptainer exec --nv \
+  --env PYTHONPATH="$HOME/.my_envs/llamafactory" \
+  --env PYTHONUSERBASE="$HOME/.my_envs/llamafactory" \
+  --bind $HOME:$HOME \
+  --bind $MY_PROJECT:$MY_PROJECT \
+  $CONTAINER_DIR/pytorch_24.09-py3.sif bash -c \
+  "source ~/.bashrc && bash run/llama3_gsm8k_sft.sh" \
+  > slurm/${prefix}_${date_str}_${SLURM_JOB_ID}.log 2>&1 
+
+echo "Job finished at $(date)"
+
+ 
