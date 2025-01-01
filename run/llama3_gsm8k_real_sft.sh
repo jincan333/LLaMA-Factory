5dbbf341979de72465f8d43451c89703e1b02ab5
jincan333 <jincan333@gmail.com>
2025-01-01 16:21:59 -0500

    revise data

--- run/llama3_gsm8k_real_sft.sh
+++ run/llama3_gsm8k_real_sft.sh
@@ -10,7 +10,7 @@ export MASTER_PORT=29501
 export OMP_NUM_THREADS=8
 
 prefix="llama3_sft"
-export dataset="gsm8k"
+export dataset="gsm8k_real_sft"
 export per_device_train_batch_size=16
 export per_device_eval_batch_size=16
 export gradient_accumulation_steps=1
---
+++ safe_reason/dpo_data.py
@@ -0,0 +1,28 @@
+import json
+
+
+data_path = 'data/gsm8k/mcts_remove_redundant.json'
+new_data_path = 'data/gsm8k/dpo.json'
+with open(data_path, 'r') as f:
+    data = json.load(f)
+
+new_data = []
+for item in data:
+    new_item = {
+        "messages": [
+            item["real"][0]
+        ],
+        "generated": [
+            item["generated"][1]
+        ],
+        "real": [
+            item["real"][1]
+        ],
+        "rectify": [
+            item["rectify"][1]
+        ]
+    }
+    new_data.append(new_item)
+
+with open(new_data_path, 'w') as f:
+    json.dump(new_data, f, indent=4)
