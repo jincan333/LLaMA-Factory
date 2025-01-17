be8206b0e1ca3bfe85ea3a8367e873b624227b33
jincan333 <jincan333@gmail.com>
2025-01-16 23:54:56 -0500

    add data

--- run_safe_reason/llama3_train_dpo.sh
+++ run_safe_reason/llama3_train_dpo.sh
@@ -10,7 +10,7 @@ export MASTER_PORT=29502
 export OMP_NUM_THREADS=8
 
 prefix="llama3_dpo"
-export dataset="gsm8k_rectify_dpo"
+export dataset="constrained_answer_dpo_meta-llama-Meta-Llama-3-8B-Instruct_4_4_50_4"
 export per_device_train_batch_size=4
 export per_device_eval_batch_size=8
 export gradient_accumulation_steps=4
@@ -29,70 +29,70 @@ envsubst < examples/train_full/llama3_full_dpo.yaml > safe_reason_logs/${experim
 FORCE_TORCHRUN=1 NNODES=1 llamafactory-cli train safe_reason_logs/${experiment_name}.yaml
 #  > logs/${experiment_name}.log 2>&1
 
-suffix="eval"
-model_name_or_path=${output_dir}
-dataset="gsm8k"
-n_shot=5
-batch_size=128
-export ACCELERATE_LOG_LEVEL=info
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-echo "evaldataset: $dataset"
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=${model_name_or_path},dtype=auto \
-    --tasks $dataset \
-    --num_fewshot $n_shot \
-    --gen_kwargs top_p=1,temperature=0 \
-    --output_path ${output_dir}/${dataset} \
-    --log_samples \
-    --write_out \
-    --apply_chat_template \
-    --wandb_args project=$current_project,name=${experiment_name}_${suffix} \
-    --batch_size $batch_size
+# suffix="eval"
+# model_name_or_path=${output_dir}
+# dataset="gsm8k"
+# n_shot=5
+# batch_size=128
+# export ACCELERATE_LOG_LEVEL=info
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# echo "evaldataset: $dataset"
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=${model_name_or_path},dtype=auto \
+#     --tasks $dataset \
+#     --num_fewshot $n_shot \
+#     --gen_kwargs top_p=1,temperature=0 \
+#     --output_path ${output_dir}/${dataset} \
+#     --log_samples \
+#     --write_out \
+#     --apply_chat_template \
+#     --wandb_args project=$current_project,name=${experiment_name}_${suffix} \
+#     --batch_size $batch_size
 
-dataset="mmlu"
-n_shot=5
-batch_size=32
-echo "evaldataset: $dataset"
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=${model_name_or_path},dtype=auto \
-    --tasks $dataset \
-    --num_fewshot $n_shot \
-    --gen_kwargs temperature=0 \
-    --output_path ${output_dir}/${dataset} \
-    --log_samples \
-    --write_out \
-    --apply_chat_template \
-    --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
-    --batch_size $batch_size
+# dataset="mmlu"
+# n_shot=5
+# batch_size=32
+# echo "evaldataset: $dataset"
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=${model_name_or_path},dtype=auto \
+#     --tasks $dataset \
+#     --num_fewshot $n_shot \
+#     --gen_kwargs temperature=0 \
+#     --output_path ${output_dir}/${dataset} \
+#     --log_samples \
+#     --write_out \
+#     --apply_chat_template \
+#     --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
+#     --batch_size $batch_size
 
-dataset="hellaswag"
-n_shot=10
-batch_size=64
-echo "evaldataset: $dataset"
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=${model_name_or_path},dtype=auto \
-    --tasks $dataset \
-    --num_fewshot $n_shot \
-    --gen_kwargs temperature=0 \
-    --output_path ${output_dir}/${dataset} \
-    --log_samples \
-    --write_out \
-    --apply_chat_template \
-    --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
-    --batch_size $batch_size
+# dataset="hellaswag"
+# n_shot=10
+# batch_size=64
+# echo "evaldataset: $dataset"
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=${model_name_or_path},dtype=auto \
+#     --tasks $dataset \
+#     --num_fewshot $n_shot \
+#     --gen_kwargs temperature=0 \
+#     --output_path ${output_dir}/${dataset} \
+#     --log_samples \
+#     --write_out \
+#     --apply_chat_template \
+#     --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
+#     --batch_size $batch_size
 
-dataset="bbh_zeroshot"
-n_shot=0
-batch_size=128
-echo "evaldataset: $dataset"
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=${model_name_or_path},dtype=auto \
-    --tasks $dataset \
-    --num_fewshot $n_shot \
-    --gen_kwargs temperature=0 \
-    --output_path ${output_dir}/${dataset} \
-    --log_samples \
-    --write_out \
-    --apply_chat_template \
-    --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
-    --batch_size $batch_size
+# dataset="bbh_zeroshot"
+# n_shot=0
+# batch_size=128
+# echo "evaldataset: $dataset"
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=${model_name_or_path},dtype=auto \
+#     --tasks $dataset \
+#     --num_fewshot $n_shot \
+#     --gen_kwargs temperature=0 \
+#     --output_path ${output_dir}/${dataset} \
+#     --log_samples \
+#     --write_out \
+#     --apply_chat_template \
+#     --wandb_args project=$current_project,name=${experiment_name}_${dataset}_${suffix} \
+#     --batch_size $batch_size
