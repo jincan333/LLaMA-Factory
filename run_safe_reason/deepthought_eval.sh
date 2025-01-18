c815130a6e0f4d66201521a3025e1edf27737272
jincan333 <jincan333@gmail.com>
2025-01-17 19:02:24 -0500

    add results

--- run_safe_reason/deepthought_eval.sh
+++ run_safe_reason/deepthought_eval.sh
@@ -31,13 +31,11 @@ experiment_name=${prefix}_${dataset}_${jailbreak}_${cot_prompt}_${model}_${judge
 # export model_name_or_path="$output_dir/$model"
 export model_name_or_path=$model_name_or_path
 
-if [ ! -f server_logs/${experiment_name}.yaml ] && [[ ${model} != *"gpt"* ]]; then
-    envsubst < examples/inference/llama3_vllm.yaml > server_logs/${experiment_name}.yaml
-    nohup script -f -a -c "API_PORT=8001 llamafactory-cli api server_logs/${experiment_name}.yaml" server_logs/${experiment_name}.log > /dev/null 2>&1 &
-    # delay=30
-    # echo "Waiting for $delay seconds..."
-    # sleep $delay
-fi
+envsubst < examples/inference/llama3_vllm.yaml > server_logs/${experiment_name}.yaml
+nohup script -f -a -c "API_PORT=8001 llamafactory-cli api server_logs/${experiment_name}.yaml" server_logs/${experiment_name}.log > /dev/null 2>&1 &
+# delay=40
+# echo "Waiting for $delay seconds..."
+# sleep $delay
 
 nohup python -u ${current_project}/evaluate.py \
     --model $model_name_or_path \
