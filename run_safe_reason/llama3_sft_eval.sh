c584ea1e92a7430dcabfd89b8f3dd38eb27a9462
jincan333 <jincan333@gmail.com>
2025-01-25 20:55:05 -0500

    train lr

--- run_safe_reason/llama3_sft_eval.sh
+++ run_safe_reason/llama3_sft_eval.sh
@@ -6,12 +6,12 @@ mkdir -p $output_dir
 echo "num_nodes: $SLURM_NNODES"
 echo "SLURM_NODEID: $SLURM_NODEID"
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 mkdir -p server_logs
 mkdir -p $log_dir
 
 prefix="inference"
-model="llama3_sft_constrained_answer_sft_preliminary_meta-llama-Meta-Llama-3-8B-Instruct_5_1_100_500_0_tbs8_ebs8_gas2_lr5e-6_cl4096"
+model="llama3_sft_sr_sft_preliminary_meta-llama-Meta-Llama-3-8B-Instruct_5_1_100_500_0_tbs8_ebs8_gas2_lr5e-6_cl4096"
 # ['llama3_sft_gsm8k_tbs16_ebs16_lr1e-6_cl2048', 'gpt-4o-mini-2024-07-18', 'gpt-4o-2024-11-20', 'llama-3-8b-instruct', 'gemma-2-9b-it']
 model_name_or_path=${output_dir}/${model}
 # ['gpt-4o-mini-2024-07-18', 'gpt-4o-2024-11-20']
